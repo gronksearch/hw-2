22b365014712a65526eba6ac7e964080e4fc5521
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-02-07 02:59:33 +0000

    Added movie model

---
+++ db/migrate/20230207025734_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string "title"
+      t.string "rated"
+      t.integer "year_released"
+      t.integer "studio_id"
+
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_025505) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_025734) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rated"
+    t.integer "year_released"
+    t.integer "studio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
