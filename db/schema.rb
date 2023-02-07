9f1c1b6915ba304670ce6bdc120acc77f14bbfd7
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-02-07 03:02:55 +0000

    Added role model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_025954) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_030116) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_025954) do
     t.string "rated"
     t.integer "year_released"
     t.integer "studio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "character_name"
+    t.integer "movie_id"
+    t.integer "actor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
